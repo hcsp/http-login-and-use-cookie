a8c90445d17d0639ab7e69837ac2cbbcd7da0960
slTrust <25893245+slTrust@users.noreply.github.com>
2019-07-31 15:44:19 +0800

    cookie (#2)
    cookie (#2)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,74 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Crawler {
-    public static String loginAndGetResponse(String username, String password) {}
+    private static String mapUserToJSONString(String username, String password) {
+        String result = "";
+        Map<String, String> map = new HashMap<>();
+        map.put("username", username);
+        map.put("password", password);
+        result = JSON.toJSONString(map);
+        return result;
+    }
+
+    public static String loginAndGetResponse(String username, String password) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://47.91.156.35:8000/auth/login");
+        httpPost.addHeader("Content-Type", "application/json;charset=UTF-8");
+        httpPost.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36");
+
+        StringEntity stringEntity = new StringEntity(mapUserToJSONString(username, password), "UTF-8");
+        stringEntity.setContentEncoding("UTF-8");
+        httpPost.setEntity(stringEntity);
+        CloseableHttpResponse response = httpclient.execute(httpPost);
+        try {
+            Header[] headers = response.getAllHeaders();
+            Map<String, String> headerMap = new HashMap<>();
+            for (Header header : headers) {
+                headerMap.put(header.getName(), header.getValue());
+            }
+            String cookie = headerMap.get("Set-Cookie");
+            String url2 = "http://47.91.156.35:8000/auth";
+            return Crawler.getContentWithCookie(url2, cookie);
+        } finally {
+            response.close();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        loginAndGetResponse("xdml", "xdml");
+    }
+
+
+    private static String getContentWithCookie(String url, String cookie) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.addHeader("Cookie", cookie);
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        try {
+            HttpEntity entity = response.getEntity();
+            String content = EntityUtils.toString(entity, "UTF-8");
+            return content;
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            response.close();
+        }
+        return null;
+    }
 }
