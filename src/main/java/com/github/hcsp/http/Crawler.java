81e292f09b448bb3ff022d91e3f67a35dbd07be6
DeeJay <1018805743@qq.com>
2019-09-07 09:05:34 +0800

    fake cookie

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -3,22 +3,16 @@ package com.github.hcsp.http;
 import com.alibaba.fastjson.JSON;
 import org.apache.commons.io.IOUtils;
 import org.apache.http.Header;
-import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Crawler {
@@ -29,7 +23,7 @@ public class Crawler {
         httpPost.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36");
         httpPost.setHeader("Content-Type", " application/json");
 
-        Map<String,String> map = new HashMap<>();
+        Map<String, String> map = new HashMap<>();
         map.put("username", username);
         map.put("password", password);
         String jsonParams = JSON.toJSONString(map);
@@ -39,7 +33,7 @@ public class Crawler {
             httpPost.setEntity(new StringEntity(jsonParams));
             response1 = httpclient.execute(httpPost);
             Header[] headers = response1.getHeaders("Set-Cookie");
-            for(Header header : headers) {
+            for (Header header : headers) {
                 String setCookie = header.getValue().split(";")[0];
                 HttpGet httpGet = new HttpGet("http://47.91.156.35:8000/auth");
                 httpGet.setHeader("Cookie", setCookie);
