fbd6eed2e23ab1411361621c1551a94ee50b5942
sceraser <scluyizhe@hotmail.com>
2019-10-11 18:10:25 +0800

    获取cookie并传递

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,71 +1,66 @@
 package com.github.hcsp.http;
 
 import com.alibaba.fastjson.JSON;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
+import java.io.InputStream;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Crawler {
-    public static String loginAndGetResponse(String username, String password) throws IOException {
-        final String url = "http://47.91.156.35:8000/auth/login";
-        final String content_type_value = "application/json";
-        final String user_agent_value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
+    private static final String url = "http://47.91.156.35:8000/auth/login";
+    private static final String url2 = "http://47.91.156.35:8000/auth";
+    private static final String content_type_value = "application/json";
+    private static final String user_agent_value = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36";
 
+    public static String loginAndGetResponse(String username, String password) throws IOException {
         CloseableHttpClient httpclient = HttpClients.createDefault();
 
         HttpPost httpPost = new HttpPost(url);
-        httpPost.setHeader("Content-Type",content_type_value);
-        httpPost.setHeader("User-Agent",user_agent_value);
+        httpPost.setHeader("Content-Type", content_type_value);
+        httpPost.setHeader("User-Agent", user_agent_value);
 
-        Map<String,String> map = new HashMap<>();
-        map.put("username","xdml");
-        map.put("password","xdml");
+
+        Map<String, String> map = new HashMap<>();
+        map.put("username", username);
+        map.put("password", password);
         String json = JSON.toJSONString(map);
-        httpPost.setEntity(new StringEntity(json,new ContentType()));
+        httpPost.setEntity(new StringEntity(json));
 
         CloseableHttpResponse response1 = httpclient.execute(httpPost);
-
         try {
-            System.out.println(response1.getStatusLine());
-            HttpEntity entity1 = response1.getEntity();
-            // do something useful with the response body
-            // and ensure it is fully consumed
-            EntityUtils.consume(entity1);
+            String cookie = response1.getFirstHeader("Set-Cookie").getValue();
+            return GetJson(cookie);
         } finally {
             response1.close();
         }
-
-//        HttpPost httpPost = new HttpPost("http://targethost/login");
-//        List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-//        nvps.add(new BasicNameValuePair("username", "vip"));
-//        nvps.add(new BasicNameValuePair("password", "secret"));
-//        httpPost.setEntity(new UrlEncodedFormEntity(nvps));
-//        CloseableHttpResponse response2 = httpclient.execute(httpPost);
-//
-//        try {
-//            System.out.println(response2.getStatusLine());
-//            HttpEntity entity2 = response2.getEntity();
-//            // do something useful with the response body
-//            // and ensure it is fully consumed
-//            EntityUtils.consume(entity2);
-//        } finally {
-//            response2.close();
-//        }
     }
+
+    public static String GetJson(String cookie_value) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+
+        HttpGet httpGet = new HttpGet(url2);
+        httpGet.setHeader("Content-Type", content_type_value);
+        httpGet.setHeader("User-Agent", user_agent_value);
+        httpGet.setHeader("Cookie", cookie_value);
+
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            HttpEntity entity1 = response1.getEntity();
+
+            InputStream is = entity1.getContent();
+
+            String str = IOUtils.toString(is, "UTF-8");
+
+            return str;
+        }
+    }
+
 }
