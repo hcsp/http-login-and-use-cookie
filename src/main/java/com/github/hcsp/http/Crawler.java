359f88eead41a2044ea32ae9b276cd7d48ca908d
emmmm <3108775859@qq.com>
2019-11-29 01:47:51 +0800

    ♣

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -10,30 +10,26 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Crawler {
-    public static String loginAndGetResponse(String username, String password) {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        CloseableHttpResponse response = null;
-        StringEntity stringEntity = null;
-        String result = null;
-        String url1 = "http://47.91.156.35:8000/auth/login";
-        String url2 = "http://47.91.156.35:8000/auth";
+    private static StringEntity stringEntity;
+    private static CloseableHttpClient httpClient = HttpClients.createDefault();
+    private static CloseableHttpResponse response = null;
 
-        // post 请求
-        HttpPost httpPost = new HttpPost(url1);
-        httpPost.addHeader("Content-Type", "application/json");
-        httpPost.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36");
-
+    public static String userToJsonString(String username,String password){
         Map<String, String> map = new HashMap();
         map.put("username", username);
         map.put("password", password);
-        String json = JSON.toJSONString(map);
+        return JSON.toJSONString(map);
+    }
+    public static void postResponse(String url,String json){
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.addHeader("Content-Type", "application/json");
+        httpPost.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.71 Safari/537.36");
 
         try {
             stringEntity = new StringEntity(json);
@@ -48,8 +44,20 @@ public class Crawler {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        // get 请求
+    public static void getResponse(String url){
+        HttpGet httpGet = new HttpGet(url);
+        httpGet.addHeader("Cookie", getCookie());
+
+        try {
+            response = httpClient.execute(httpGet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getCookie(){
         Header[] httpHeaders = response.getAllHeaders();
         String cookie = null;
         for (Header i : httpHeaders) {
@@ -58,27 +66,35 @@ public class Crawler {
                 break;
             }
         }
-
         cookie = cookie.split(";")[0];
+        return cookie;
+    }
 
-        HttpGet httpGet = new HttpGet(url2);
-        httpGet.addHeader("Cookie", cookie);
-
-        try {
-            response = httpClient.execute(httpGet);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+    public static String getResultString(){
+        String result = null;
         HttpEntity entity = response.getEntity();
-
         try {
             result = EntityUtils.toString(entity);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return result;
+    }
+
+    public static String loginAndGetResponse(String username, String password) {
+        String url1 = "http://47.91.156.35:8000/auth/login";
+        String url2 = "http://47.91.156.35:8000/auth";
+
+        // USER_TO_JSON
+        String json = userToJsonString(username, password);
+
+        // POST
+        postResponse(url1,json);
+
+        // GET
+        getResponse(url2);
+
+        return getResultString();
     }
 }
 
