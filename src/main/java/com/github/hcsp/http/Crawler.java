c97a1c62cd216ffc75d163c78eea14d735fc6af5
ws1603 <48832455+ws1603@users.noreply.github.com>
2019-11-28 16:43:50 +0800

    使用Java代码进行模拟登录 (#54)
    使用Java代码进行模拟登录 (#54)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,52 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class Crawler {
-    public static String loginAndGetResponse(String username, String password) {}
+    public static String loginAndGetResponse(String username, String password) throws IOException {
+        Map<String, String> map = new HashMap<>();
+        map.put("username", username);
+        map.put("password", password);
+        String json = JSON.toJSONString(map);
+        String url = "http://47.91.156.35:8000/auth/login";
+        HttpPost httpPost = new HttpPost(url);
+        StringEntity entity = new StringEntity(json);
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Content-Type", "application/json");
+        httpPost.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.87 Safari/537.36");
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpResponse response = httpclient.execute(httpPost);
+        System.out.println(response.getStatusLine());
+        String cookie = response.getFirstHeader("Set-Cookie").getValue();
+        System.out.println(cookie);
+        response.close();
+        return getJson(cookie);
+    }
+
+
+    public static String getJson(String cookie) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://47.91.156.35:8000/auth");
+        httpGet.addHeader("Cookie", cookie);
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        System.out.println(response1.getStatusLine());
+        InputStream is = response1.getEntity().getContent();
+        String content = IOUtils.toString(is, "utf-8");
+        response1.close();
+        return content;
+    }
 }
+
