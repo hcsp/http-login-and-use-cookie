415f99c7d81f931f01da1f03f1506e01a6c0bf20
ayang818 <ayang818@qq.com>
2019-12-21 14:00:05 +0800

    fix unknow bug

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -16,35 +16,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Crawler {
-    public static String loginAndGetResponse(String username, String password) throws IOException {
+    public static String loginAndGetResponse(String username, String password) {
         Map<String, String> authDto = new HashMap<>();
         authDto.put("username", username);
         authDto.put("password", password);
         String authJson = JSON.toJSONString(authDto);
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpPost httpPost = new HttpPost("http://47.91.156.35:8000/auth/login");
-        httpPost.addHeader("Content-Type", "application/json");
-        httpPost.addHeader("User-Agent", "Mozilla/5.0");
-        StringEntity entity = new StringEntity(authJson, "UTF-8");
-        httpPost.setEntity(entity);
-        CloseableHttpResponse response = client.execute(httpPost);
-        Header[] headers = response.getHeaders("Set-Cookie");
-        String[] headerString = new String[0];
-        if (headers.length == 1) {
-            headerString = headers[0].toString().split("; ");
-        }
-        String jSessionId = "";
-        if (headerString.length != 0) {
-            String[] split = headerString[0].split("=");
-            jSessionId = split[split.length - 1];
-        }
+        String body = null;
+        try (CloseableHttpClient client = HttpClients.createDefault()) {
+            HttpPost httpPost = new HttpPost("http://47.91.156.35:8000/auth/login");
+            httpPost.addHeader("Content-Type", "application/json");
+            httpPost.addHeader("User-Agent", "Mozilla/5.0");
+            StringEntity entity = new StringEntity(authJson, "UTF-8");
+            httpPost.setEntity(entity);
+            CloseableHttpResponse response = null;
+            response = client.execute(httpPost);
+            Header[] headers = response.getHeaders("Set-Cookie");
+            String[] headerString = new String[0];
+            if (headers.length == 1) {
+                headerString = headers[0].toString().split("; ");
+            }
+            String jSessionId = "";
+            if (headerString.length != 0) {
+                String[] split = headerString[0].split("=");
+                jSessionId = split[split.length - 1];
+            }
 
-        HttpGet httpGet = new HttpGet("http://47.91.156.35:8000/auth");
-        httpGet.addHeader("JSESSIONID", jSessionId);
-        CloseableHttpResponse verifyAuthResponse = client.execute(httpGet);
-        ResponseHandler<String> handler = new BasicResponseHandler();
-        String body = handler.handleResponse(verifyAuthResponse);
-        client.close();
+            HttpGet httpGet = new HttpGet("http://47.91.156.35:8000/auth");
+            httpGet.addHeader("JSESSIONID", jSessionId);
+            CloseableHttpResponse verifyAuthResponse = client.execute(httpGet);
+            ResponseHandler<String> handler = new BasicResponseHandler();
+            body = handler.handleResponse(verifyAuthResponse);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return body;
     }
 }
