83fdbad1e0628f4099f28307399701f6162a3e91
NervousOrange <zch693922442@outlook.com>
2020-01-04 22:55:29 +0100

    from NervousOrange: Assignment of HTTP foundation. 2

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -2,14 +2,12 @@ package com.github.hcsp.http;
 
 import com.alibaba.fastjson.JSON;
 import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
