d296d62b3f2a93f74d195f388b8f050b4f31e46a
zaker-666 <1075151259china@gmail.com>
2020-01-07 15:52:38 +0800

    使用cookies

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -13,27 +13,28 @@ import java.util.Map;
 
 public class Crawler {
     public static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+
     public static String loginAndGetResponse(String username, String password) {
         String header = "";
         String result = "";
-        Map<String ,String> login = new HashMap<>();
-        login.put("username","xdml");
-        login.put("password","xdml");
+        Map<String, String> login = new HashMap<>();
+        login.put("username", "xdml");
+        login.put("password", "xdml");
         String logInfo = JSONObject.toJSONString(login);
         OkHttpClient okHttpClient = new OkHttpClient();
         RequestBody requestBody = RequestBody.create(logInfo, JSON);
         Request loginTry = new Request.Builder().post(requestBody)
                 .url("http://47.91.156.35:8000/auth/login")
-                .addHeader("Content-Type","application/json")
-                .addHeader("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36")
+                .addHeader("Content-Type", "application/json")
+                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36")
                 .build();
-        try(Response execute = okHttpClient.newCall(loginTry).execute()) {
+        try (Response execute = okHttpClient.newCall(loginTry).execute()) {
             header = execute.header("Set-Cookie");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        Request secondlogin = new Request.Builder().get().url("http://47.91.156.35:8000/auth").addHeader("Cookie",header).build();
-        try(Response execute = okHttpClient.newCall(secondlogin).execute()) {
+        Request secondlogin = new Request.Builder().get().url("http://47.91.156.35:8000/auth").addHeader("Cookie", header).build();
+        try (Response execute = okHttpClient.newCall(secondlogin).execute()) {
             InputStream inputStream = execute.body().byteStream();
             result = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
         } catch (IOException e) {
