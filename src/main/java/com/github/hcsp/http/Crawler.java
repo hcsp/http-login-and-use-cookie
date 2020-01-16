f1a449efbca38f0711f2de441af9c8db114ae0e7
hukai <huzhengen@163.com>
2020-01-16 13:00:34 +0800

    使用Java代码进行模拟登录

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,51 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Crawler {
-    public static String loginAndGetResponse(String username, String password) {}
+    public static String loginAndGetResponse(String username, String password) throws IOException {
+        String cookie = getCookie(username, password);
+        return getLoginStatus(cookie);
+    }
+
+    private static String getLoginStatus(String cookie) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://47.91.156.35:8000/auth");
+        httpGet.addHeader("Cookie", cookie);
+        try (CloseableHttpResponse response = httpclient.execute(httpGet)) {
+            System.out.println(response);
+            System.out.println(response.getStatusLine());
+            return IOUtils.toString(response.getEntity().getContent(), StandardCharsets.UTF_8);
+        }
+    }
+
+    private static String getCookie(String username, String password) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://47.91.156.35:8000/auth/login");
+        httpPost.addHeader("Content-Type", "application/json");
+        httpPost.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36");
+        Map<String, String> map = new HashMap<>();
+        map.put("username", username);
+        map.put("password", password);
+        httpPost.setEntity(new StringEntity(JSON.toJSONString(map)));
+        CloseableHttpResponse response = httpclient.execute(httpPost);
+        return response.getHeaders("Set-Cookie")[0].getValue().split(";")[0];
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        Crawler.loginAndGetResponse("xdml", "xdml");
+    }
 }
