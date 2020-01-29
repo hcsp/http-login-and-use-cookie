60b488e41da76e43bfcabfa09ba10508b1cbe787
Felix <fiymi666@gmail.com>
2020-01-29 11:24:40 +0800

    post登录之后，get返回json字符串

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,62 @@
 package com.github.hcsp.http;
 
+
+import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+
 public class Crawler {
-    public static String loginAndGetResponse(String username, String password) {}
+    public static String loginAndGetResponse(String username, String password) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://47.91.156.35:8000/auth/login");
+        httpPost.addHeader("Content-Type", "application/json");
+        httpPost.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36");
+        HashMap<String, Object> nvps = new HashMap<>();
+        nvps.put("username", username);
+        nvps.put("password", password);
+        JSONObject json = new JSONObject(nvps);
+        StringEntity newjson = new StringEntity(json.toString());
+        httpPost.setEntity(newjson);
+        CloseableHttpResponse response2 = httpclient.execute(httpPost);
+
+        try {
+            System.out.println(response2.getStatusLine());
+//            String cookie= response2.getFirstHeader("Set-Cookie").toString();
+            HttpEntity entity2 = response2.getEntity();
+            // do something useful with the response body
+            // and ensure it is fully consumed
+            EntityUtils.consume(entity2);
+        } finally {
+            response2.close();
+        }
+        String cookie = response2.getFirstHeader("Set-Cookie").toString().substring(12, 55);
+
+        HttpGet httpGet = new HttpGet("http://47.91.156.35:8000/auth");
+        httpGet.addHeader("Cookie", cookie);
+        CloseableHttpResponse response1 = httpclient.execute(httpGet);
+        HttpEntity entity1 = response1.getEntity();
+        InputStream content = entity1.getContent();
+        String result = IOUtils.toString(content, StandardCharsets.UTF_8);
+        try {
+            System.out.println(response1.getStatusLine());
+            EntityUtils.consume(entity1);
+        } finally {
+            response1.close();
+        }
+
+        return result;
+    }
 }
