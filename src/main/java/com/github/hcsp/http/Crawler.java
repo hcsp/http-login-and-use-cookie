2aa35a4275bc7c3f1b2cdfd59c4ddcb13f8f0ba7
lipeng <feiyangtianxitian@gmail.com>
2020-03-23 22:14:16 +0800

    HTTP header/body/cookie 实战

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,19 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Crawler {
     public static String loginAndGetResponse(String username, String password) {}
 }
