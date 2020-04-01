0dc5ef043bbcd7e27ac70e151b26b2da75ee0103
Mcavoy GENG <gigi834921748@qq.com>
2020-04-01 22:57:05 +0100

    http-login-and-use-cookie (#122)
    http-login-and-use-cookie (#122)

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,74 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSON;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static java.util.regex.Pattern.*;
+
 public class Crawler {
-    public static String loginAndGetResponse(String username, String password) {}
+    public static String loginAndGetResponse(String username, String password) throws IOException {
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost("http://47.91.156.35:8000/auth/login");
+        Map<String, String> map = new HashMap<>();
+        map.put("username", username);
+        map.put("password", password);
+
+        String json = JSON.toJSONString(map);
+
+        StringEntity entity = new StringEntity(json);
+        httpPost.setEntity(entity);
+        httpPost.setHeader("Accept", "application/json");
+        httpPost.setHeader("Content-type", "application/json");
+        httpPost.addHeader("User-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.79 Safari/537.36");
+
+        CloseableHttpResponse response = client.execute(httpPost);
+        //get all headers
+        Header[] headers = response.getAllHeaders();
+        for (Header header : headers) {
+            System.out.println("Key : " + header.getName()
+                    + " ,Value : " + header.getValue());
+        }
+        String cookie = response.getFirstHeader("set-cookie").getValue();
+        Pattern regex = compile("\\w+=\\w+");
+        Matcher m = regex.matcher(cookie);
+        if (m.find()) {
+            String session = m.group(0);
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet("http://47.91.156.35:8000/auth/");
+            httpGet.addHeader("Cookie", session);
+            try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+                HttpEntity entity1 = response1.getEntity();
+                InputStream is = entity1.getContent();
+                return IOUtils.toString(is, StandardCharsets.UTF_8);
+
+
+            }
+        }
+
+        client.close();
+        return "";
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        String result = loginAndGetResponse("xdml", "xdml");
+
+
+    }
 }
