da90edbc990b181cc14efc05e44f12f790093146
wuzhenquan <782754354@qq.com>
2020-05-21 22:50:22 +0800

    HTTP实战: 使用Java代码进行模拟登录

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -21,52 +21,59 @@ import java.util.Map;
 public class Crawler {
 
     public static String loginAndGetResponse(String username, String password) throws IOException {
-        String JSESSIONID;
+        String loginUrl = "http://47.91.156.35:8000/auth/login";
         CloseableHttpClient httpclient = HttpClients.createDefault();
-
-        HttpPost httpPost = new HttpPost("http://47.91.156.35:8000/auth/login");
+        HttpPost httpPost = new HttpPost(loginUrl);
         httpPost.addHeader("Content-Type", "application/json");
-        httpPost.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36");
-        Map<String, String> map = new HashMap();
-        map.put("username", "xdml");
-        map.put("password", "xdml");
+        httpPost.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36");
+        //JSON.toJSONString是将对象转化为Json字符串//body
+        Map<String, String> map = new HashMap<>();
+        map.put("username", username);
+        map.put("password", password);
         String json = JSON.toJSONString(map);
+        //HttpEntity其实相当于一个消息实体，内容是http传送的报文
         HttpEntity entity = new StringEntity(json);
+        //使用HttpPost方法提交HTTP POST请求，则需要使用HttpPost类的setEntity方法设置请求参数
+        //这里就是将用户名以及密码设为请求参数
         httpPost.setEntity(entity);
-        CloseableHttpResponse response2 = httpclient.execute(httpPost);
-
+        String JSESSIONID = null;
+        //得到post请求的响应
+        CloseableHttpResponse response = httpclient.execute(httpPost);
         try {
-            HttpEntity entity2 = response2.getEntity();
-            InputStream is = entity2.getContent();
-            String html = IOUtils.toString(is,"UTF-8");
-            // do something useful with the response body
-            // and ensure it is fully consumed
-            Header setCookieHeader = response2.getFirstHeader("Set-Cookie");
+            System.out.println(response.getStatusLine());
+            HttpEntity responseEntity = response.getEntity();
+            String responseStr = IOUtils.toString(responseEntity.getContent());
+            System.out.println(responseStr);
+
+            Header[] headers = response.getAllHeaders();
+            for (Header header : headers) {
+                System.out.println(header.toString());
+            }
+            Header setCookieHeader = response.getFirstHeader("Set-Cookie");
             JSESSIONID = setCookieHeader.getValue().split("; ")[0];
-            EntityUtils.consume(entity2);
+
         } finally {
-            response2.close();
+            response.close();
         }
+        //返回使用COOKIE发送请求的响应
         return getResponseWithJSESSIONID(JSESSIONID);
     }
 
     private static String getResponseWithJSESSIONID(String JSESSIONID) throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpClient httpClient2 = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet("http://47.91.156.35:8000/auth");
         httpGet.addHeader("Cookie", JSESSIONID);
 
-        CloseableHttpResponse response = httpclient.execute(httpGet);
+        CloseableHttpResponse response2 = httpClient2.execute(httpGet);
         try {
-            System.out.println(response.getStatusLine());
-            HttpEntity entity1 = response.getEntity();
-            // do something useful with the response body
-            // and ensure it is fully consumed
+            System.out.println(response2.getStatusLine());
+            HttpEntity entity1 = response2.getEntity();
 
-            String responseStr = IOUtils.toString(entity1.getContent(), StandardCharsets.UTF_8);
-            System.out.println(responseStr);
-            return responseStr;
+            String responseStr2 = IOUtils.toString(entity1.getContent(), "UTF-8");
+            System.out.println(responseStr2);
+            return responseStr2;
         } finally {
-            response.close();
+            response2.close();
         }
     }
 
