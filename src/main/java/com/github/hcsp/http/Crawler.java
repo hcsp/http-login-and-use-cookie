13c1fb8176d0e517d22ca654922c8bb1f8b7d87c
kongmu373 <kongmu1235@gmail.com>
2020-05-28 19:49:11 +0800

    重构.

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -2,7 +2,6 @@ package com.github.hcsp.http;
 
 import com.alibaba.fastjson.JSON;
 import org.apache.commons.io.IOUtils;
-import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
@@ -21,7 +20,32 @@ public class Crawler {
     private static final String AUTH = "http://47.91.156.35:8000/auth";
 
     public static String loginAndGetResponse(String username, String password) throws IOException {
-        CloseableHttpClient httpclient = HttpClients.createDefault();
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        Map<String, String> map = new HashMap<>();
+        map.put("username", username);
+        map.put("password", password);
+        String json = JSON.toJSONString(map);
+        String cookie = post(json);
+        return get(cookie);
+    }
+
+    private static String get(String cookie) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(AUTH);
+        // add Headers
+        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36");
+        httpGet.addHeader("Content-Type", "application/json");
+        httpGet.addHeader("Cookie", cookie);
+        String result;
+        try (CloseableHttpResponse getResponse = httpClient.execute(httpGet)) {
+            InputStream content = getResponse.getEntity().getContent();
+            result = IOUtils.toString(content, StandardCharsets.UTF_8);
+        }
+        return result;
+    }
+
+    private static String post(String json) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost(LOGIN);
         // add Headers
         httpPost.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36");
@@ -29,37 +53,15 @@ public class Crawler {
 
         BasicHttpEntity basicHttpEntity = new BasicHttpEntity();
 
-        Map<String, String> map = new HashMap<>();
-        map.put("username", username);
-        map.put("password", password);
-        String jsonString = JSON.toJSONString(map);
 
-        InputStream stream = IOUtils.toInputStream(jsonString, StandardCharsets.UTF_8);
+        InputStream stream = IOUtils.toInputStream(json, StandardCharsets.UTF_8);
         basicHttpEntity.setContent(stream);
 
         httpPost.setEntity(basicHttpEntity);
 
-        CloseableHttpResponse postResponse = null;
-        CloseableHttpResponse getResponse = null;
-        String result = "";
-        try {
-            postResponse = httpclient.execute(httpPost);
-            Header[] cookieHeaders = postResponse.getHeaders("Set-Cookie");
-            if (cookieHeaders != null && cookieHeaders.length > 0) {
-                String JSESSIONID = cookieHeaders[0].getValue();
-                HttpGet httpGet = new HttpGet(AUTH);
-                httpGet.setHeader("Cookie", JSESSIONID);
-                getResponse = httpclient.execute(httpGet);
-                InputStream content = getResponse.getEntity().getContent();
-                result = IOUtils.toString(content, StandardCharsets.UTF_8);
-            }
-        } finally {
-            if (postResponse != null) {
-                postResponse.close();
-            }
-            if (getResponse != null) {
-                getResponse.close();
-            }
+        String result;
+        try (CloseableHttpResponse postResponse = httpClient.execute(httpPost)) {
+            result = postResponse.getHeaders("Set-Cookie")[0].getValue();
         }
         return result;
     }
