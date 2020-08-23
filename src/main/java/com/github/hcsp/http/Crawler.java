067a482bba9415eb517a0df421a9b7f175b86d16
Nat Chen <chen_natalie@outlook.com>
2020-08-23 20:32:14 +0800

    fetch request with cookie

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,5 +1,55 @@
 package com.github.hcsp.http;
 
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class Crawler {
-    public static String loginAndGetResponse(String username, String password) {}
+    public static String loginAndGetResponse(String username, String password) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        String cookie = null;
+        HttpPost httpPost = new HttpPost("http://47.91.156.35:8000/auth/login");
+        httpPost.setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36");
+        httpPost.setHeader("Content-Type", "application/json");
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username", username);
+        jsonObject.put("password", password);
+        httpPost.setEntity(new StringEntity(jsonObject.toJSONString()));
+        CloseableHttpResponse response = httpclient.execute(httpPost);
+
+        try {
+            HttpEntity entity = response.getEntity();
+            Header setCookieHeader = response.getFirstHeader("Set-Cookie");
+            cookie = setCookieHeader.getValue().split("; ")[0];
+            EntityUtils.consume(entity);
+        } finally {
+            response.close();
+        }
+
+        return cookie;
+    }
+
+    public static String fetchRequestWithCookie(String cookie) throws IOException {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://47.91.156.35:8000/auth");
+        httpGet.setHeader("Set-Cookie", cookie);
+        httpGet.setHeader("Content-Type", "application/json");
+        CloseableHttpResponse response = httpclient.execute(httpGet);
+        try {
+            HttpEntity entity = response.getEntity();
+            return EntityUtils.toString(entity, StandardCharsets.UTF_8);
+        } finally {
+            response.close();
+        }
+    }
 }
