80137b6f77f0ce4676c7f1726c805a4b6f8ca010
alex <douknowtheshy@gmail.com>
2020-12-15 21:44:54 -0800

    22

--- src/main/java/com/github/hcsp/http/Crawler.java
+++ src/main/java/com/github/hcsp/http/Crawler.java
@@ -1,9 +1,12 @@
 package com.github.hcsp.http;
 
 import com.alibaba.fastjson.JSON;
-import org.apache.http.Header;
+
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
+
 import org.apache.http.client.methods.CloseableHttpResponse;
+
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
@@ -12,46 +15,64 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Crawler {
-    public static String loginAndGetResponse(String username, String password) throws IOException {
-        String cookie = null;
-        CloseableHttpClient httpclient = HttpClients.createDefault();
+    static String cookie;
 
+    public static String loginAndGetResponse(String username, String password) throws IOException {
+        String request;
+
+        CloseableHttpClient httpclient = HttpClients.createDefault();
         HttpPost httpPost = new HttpPost("http://47.91.156.35:8000/auth/login");
-        httpPost.addHeader("Content-Type", "application/json");
-        httpPost.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36");
+
 
         Map<String, String> map = new HashMap<>();
         map.put("username", username);
         map.put("password", password);
         String json = JSON.toJSONString(map);
-        httpPost.setEntity(new StringEntity(json));
+        StringEntity stringEntity = new StringEntity(json, "UTF-8");
+        httpPost.setEntity(stringEntity);
 
-        CloseableHttpResponse response1 = httpclient.execute(httpPost);
+
+        httpPost.addHeader("Content-Type", "application/json");
+        httpPost.addHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11");
+
+        CloseableHttpResponse response = httpclient.execute(httpPost);
+
         try {
-            Header header = response1.getFirstHeader("Set-Cookie");
-            HttpEntity entity = response1.getEntity();
-            cookie = header.getValue().split(";")[0];
-            EntityUtils.consume(entity);
+            System.out.println(response.getStatusLine());
+            HttpEntity httpEntity = response.getEntity();
+
+            InputStream inputStream = httpEntity.getContent();
+            request = IOUtils.toString(inputStream, "UTF-8");
+            String[] headers = response.getFirstHeader("Set-Cookie").getValue().split(";");
+            cookie = headers[0];
+
+        } finally {
+            httpclient.close();
+        }
+        return request;
+    }
+
+    public static String HttpGetQuest() throws IOException {
+        String result;
+        CloseableHttpClient httpclientQuest = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://47.91.156.35:8000/auth");
+        httpGet.setHeader("Cookie", cookie);
+        CloseableHttpResponse response1 = httpclientQuest.execute(httpGet);
+        try {
+            System.out.println(response1.getStatusLine());
+            HttpEntity entity1 = response1.getEntity();
+
+            result=IOUtils.toString(entity1.getContent(), "UTF-8");
+            System.out.println(result);
+            EntityUtils.consume(entity1);
         } finally {
             response1.close();
         }
-
-        HttpGet httpGet = new HttpGet("http://47.91.156.35:8000/auth");
-        httpGet.addHeader("Cookie", cookie);
-        httpGet.addHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.94 Safari/537.36");
-
-        CloseableHttpResponse response2 = httpclient.execute(httpGet);
-        try {
-            HttpEntity entity2 = response2.getEntity();
-            String headerBody = EntityUtils.toString(entity2);
-            EntityUtils.consume(entity2);
-            return headerBody;
-        } finally {
-            response2.close();
-        }
+        return result;
     }
 }
